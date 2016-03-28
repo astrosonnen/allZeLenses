cf904ce3087b2f1af4c2a2962bc6863d5da303df
Alessandro Sonnenfeld <sonnen@physics.ucsb.edu>
2016-03-27 23:43:50 -0700

    added method to do the fit using the radio of radial magnifications as well

--- lens_samplers.py
+++ lens_samplers.py
@@ -16,7 +16,123 @@ def fit_nfwdev_nocvirscat_nodtfit(lens, nstep=15000, burnin=5000, thin=1):
     imerr = lens.obs_images[1]
 
     mstar_obs, mstar_err = lens.obs_lmstar
-    dt_obs, dt_err = lens.obs_timedelay
+
+    radmagrat_obs, radmagrat_err = lens.obs_radmagrat
+
+    model_lens.get_caustic()
+    model_lens.make_grids(err=imerr, nsig=5.)
+
+    mstar_par = pymc.Uniform('lmstar', lower=10.5, upper=12.5, value=np.log10(lens.mstar))
+    alpha_par = pymc.Uniform('alpha', lower=-0.5, upper=0.5, value=0.)
+    mhalo_par = pymc.Uniform('mhalo', lower=12., upper=14., value=np.log10(lens.mhalo))
+
+    def cfunc(lmhalo):
+        return 10.**(0.971 - 0.094*(lmhalo - 12.))
+
+    @pymc.deterministic()
+    def caustic(mstar=mstar_par, mhalo=mhalo_par):
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = cfunc(mhalo)
+
+        model_lens.normalize()
+
+        model_lens.get_caustic()
+
+        return model_lens.caustic
+
+    s2_par = pymc.Uniform('s2', lower=0., upper=caustic**2, value=lens.source**2)
+
+    @pymc.deterministic()
+    def images(mstar=mstar_par, mhalo=mhalo_par, s2=s2_par):
+
+        model_lens.source = s2**0.5
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = cfunc(mhalo)
+
+        model_lens.normalize()
+
+        model_lens.get_images()
+        if len(model_lens.images) < 2:
+            return np.inf, -np.inf
+        else:
+            return model_lens.images
+
+    @pymc.deterministic()
+    def image_a(imgs=images):
+        return imgs[0]
+
+    @pymc.deterministic()
+    def image_b(imgs=images):
+        return imgs[1]
+
+    @pymc.deterministic()
+    def radmagrat(mstar=mstar_par, mhalo=mhalo_par, s2=s2_par, imgs=images):
+
+        model_lens.source = s2**0.5
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = cfunc(mhalo)
+
+        model_lens.normalize()
+        model_lens.images = imgs
+
+        if len(imgs) < 2:
+            return 0.
+        else:
+            model_lens.get_radmag_ratio()
+            return model_lens.radmag_ratio
+
+    @pymc.deterministic()
+    def timedelay(mstar=mstar_par, mhalo=mhalo_par, s2=s2_par, imgs=images):
+
+        model_lens.source = s2**0.5
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = cfunc(mhalo)
+
+        model_lens.normalize()
+        model_lens.images = imgs
+
+        if len(imgs) < 2:
+            return 0.
+        else:
+            model_lens.get_timedelay()
+            return model_lens.timedelay
+
+    ima_logp = pymc.Normal('ima_logp', mu=image_a, tau=1./imerr**2, value=xa_obs, observed=True)
+    imb_logp = pymc.Normal('imb_logp', mu=image_b, tau=1./imerr**2, value=xb_obs, observed=True)
+
+    msps_logp = pymc.Normal('msps_logp', mu=mstar_par-alpha_par, tau=1./mstar_err**2, value=mstar_obs, observed=True)
+
+    radmag_logp = pymc.Normal('radmag_logp', mu=radmagrat, tau=1./radmagrat_err**2, value=radmagrat_obs, observed=True)
+
+    pars = [mstar_par, mhalo_par, alpha_par, s2_par, timedelay, image_a, image_b, caustic, radmagrat]
+
+    M = pymc.MCMC(pars)
+    M.use_step_method(pymc.AdaptiveMetropolis, [mstar_par, mhalo_par, alpha_par, s2_par])
+    M.sample(nstep, burnin, thin=thin)
+
+    outdic = {'mstar':M.trace('lmstar')[:], 'mhalo':M.trace('mhalo')[:], 'alpha': M.trace('alpha')[:], \
+              'timedelay':M.trace('timedelay')[:], 'source':(M.trace('s2')[:]**0.5).flatten(), \
+              'image_a':M.trace('image_a')[:], 'image_b':M.trace('image_b')[:], 'caustic': M.trace('caustic')[:],\
+              'radmagrat': M.trace('radmagrat')}
+
+    return outdic
+
+
+def fit_nfwdev_nocvirscat_noradmagfit_nodtfit(lens, nstep=15000, burnin=5000, thin=1):
+
+    model_lens = lens_models.NfwDev(zd=lens.zd, zs=lens.zs, mstar=lens.mstar, mhalo=lens.mhalo, \
+                                    reff_phys=lens.reff_phys, cvir=lens.cvir, images=lens.images, source=lens.source)
+
+    model_lens.normalize()
+
+    xa_obs, xb_obs = lens.obs_images[0]
+    imerr = lens.obs_images[1]
+
+    mstar_obs, mstar_err = lens.obs_lmstar
 
     model_lens.get_caustic()
     model_lens.make_grids(err=imerr, nsig=5.)
