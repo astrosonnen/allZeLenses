d021913293e80be6ddb44fb980404f0199ce81e3
Alessandro Sonnenfeld <sonnen@physics.ucsb.edu>
2016-03-30 21:49:26 -0700

    added Gaussian interim prior on mhalo

--- toy_models/hierarchical_inference.py
+++ toy_models/hierarchical_inference.py
@@ -5,6 +5,87 @@ import pickle
 day = 24.*3600.
 
 
+def infer_simple_reality_interimprior(guess, chains, dt_obs, dt_err, nstep=15000, burnin=5000, thin=1):
+
+    from scipy.special import erf
+
+    nlens = len(chains)
+
+    # goes through the chains and thins them to nis samples
+    tchains = []
+    for i in range(nlens):
+        chain = chains[i]
+
+        for par in chain:
+            nsamp = len(chain[par])
+            keep = thin*np.arange(nsamp/thin)
+            chain[par] = chain[par].flatten()[keep]
+
+        bad = chain['timedelay'] != chain['timedelay']
+        chain['timedelay'][bad.flatten()] = 0.
+
+        tchains.append(chain)
+
+    #defines the hyper-parameters
+
+    mhalo_mu = pymc.Uniform('mhalo_mu', lower=12.0, upper=14.0, value=guess['mhalo_mu'])
+    mhalo_sig = pymc.Uniform('mhalo_sig', lower=0., upper=1., value=guess['mhalo_sig'])
+
+    mstar_mhalo = pymc.Uniform('mstar_mhalo', lower=0., upper=2., value=guess['mstar_mhalo'])
+
+    mstar_mu = pymc.Uniform('mstar_mu', lower=11., upper=12., value=guess['mstar_mu'])
+    mstar_sig = pymc.Uniform('mstar_sig', lower=0., upper=2., value=guess['mstar_sig'])
+
+    aimf_mu = pymc.Uniform('aimf_mu', lower=-0.2, upper=0.2, value=guess['aimf_mu'])
+    aimf_sig = pymc.Uniform('aimf_sig', lower=0., upper=0.5, value=guess['aimf_sig'])
+
+    h70 = pymc.Uniform('h70', lower=0.5, upper=1.5, value=1.)
+
+    pars = [mhalo_mu, mhalo_sig, mstar_mhalo, mstar_mu, mstar_sig, aimf_mu, aimf_sig, h70]
+
+    @pymc.deterministic(name='like')
+    def like(mhalo_mu=mhalo_mu, mhalo_sig=mhalo_sig, mstar_mhalo=mstar_mhalo, mstar_mu=mstar_mu, mstar_sig=mstar_sig, \
+             aimf_mu=aimf_mu, aimf_sig=aimf_sig, h70=h70):
+
+        totlike = 0.
+
+        for i in range(nlens):
+            mglob_model = mstar_mu + mstar_mhalo*(tchains[i]['mhalo'] - 13.)
+
+            mh_term = 1./mhalo_sig*np.exp(-(mhalo_mu - tchains[i]['mhalo'])**2/(2.*mhalo_sig**2))
+            ms_term = 1./mstar_sig*np.exp(-(mglob_model - tchains[i]['mstar'])**2/(2.*mstar_sig**2))
+            a_term = 1./aimf_sig*np.exp(-0.5*(aimf_mu - tchains[i]['alpha'])**2/aimf_sig**2)
+
+            interim_prior = 1./0.3**2*np.exp(-0.5*(tchains[i]['mhalo'] - 1.7 - tchains[i]['mstar'])**2/0.3**2)
+
+            dt_term = 1./dt_err[i]*np.exp(-0.5*(dt_obs[i] - tchains[i]['timedelay']/h70)**2/dt_err[i]**2)
+
+            #norms = 0.5*(erf((mglob_model - 10.5)/2.**0.5/mstar_sig) - erf((mglob_model - 12.5)/2.**0.5/mstar_sig)) * \
+            #        0.5*(erf((mhalo_mu - 12.)/2.**0.5/mhalo_sig) - erf((mhalo_mu - 14.)/2.**0.5/mhalo_sig)) * \
+            #        0.5*(erf((aimf_mu + 0.5)/2.**0.5/aimf_sig) - erf((aimf_mu - 0.5)/2.**0.5/aimf_sig))
+            norms = 1.
+
+            term = (mh_term*ms_term*a_term*dt_term/norms/interim_prior).mean()
+            totlike += np.log(term)
+
+        return totlike
+
+    @pymc.stochastic(observed=True, name='logp')
+    def logp(value=0., p=pars):
+        return like
+
+    M = pymc.MCMC(pars+[like])
+    M.use_step_method(pymc.AdaptiveMetropolis, pars)
+    M.sample(nstep, burnin)
+
+    outdic = {}
+    for par in pars:
+        outdic[str(par)] = M.trace(par)[:]
+    outdic['logp'] = M.trace('like')[:]
+
+    return outdic
+
+
 def infer_simple_reality(guess, chains, dt_obs, dt_err, nstep=15000, burnin=5000, thin=1):
 
     from scipy.special import erf
@@ -59,8 +140,8 @@ def infer_simple_reality(guess, chains, dt_obs, dt_err, nstep=15000, burnin=5000
             dt_term = 1./dt_err[i]*np.exp(-0.5*(dt_obs[i] - tchains[i]['timedelay']/h70)**2/dt_err[i]**2)
 
             #norms = 0.5*(erf((mglob_model - 10.5)/2.**0.5/mstar_sig) - erf((mglob_model - 12.5)/2.**0.5/mstar_sig)) * \
-            #        0.5*(erf((mhalo_mu - 12.)/2.**0.5/mhalo_sig) - erf((mhalo_mu - 14.)/2.**0.5/mhalo_sig))
-
+            #        0.5*(erf((mhalo_mu - 12.)/2.**0.5/mhalo_sig) - erf((mhalo_mu - 14.)/2.**0.5/mhalo_sig)) * \
+            #        0.5*(erf((aimf_mu + 0.5)/2.**0.5/aimf_sig) - erf((aimf_mu - 0.5)/2.**0.5/aimf_sig))
             norms = 1.
 
             term = (mh_term*ms_term*a_term*dt_term/norms).mean()
@@ -169,6 +250,9 @@ def infer_simple_reality_knownimf(guess, chains, dt_obs, dt_err, nstep=11000, bu
             nsamp = len(chain[par])
             keep = thin*np.arange(nsamp/thin)
             chain[par] = chain[par].flatten()[keep]
+
+        bad = chain['timedelay'] != chain['timedelay']
+        chain['timedelay'][bad.flatten()] = 0.
 
         tchains.append(chain)
 
