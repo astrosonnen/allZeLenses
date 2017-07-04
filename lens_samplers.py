d47998dcd7e1a693e0ee02db85d0df9772a3ff93
astrosonnen <alessandro.sonnenfeld@ipmu.jp>
2017-07-05 04:50:37 +0900

    added mstarmhaloprior function

--- lens_samplers.py
+++ lens_samplers.py
@@ -120,6 +120,121 @@ def fit_nfwdev_interimprior(lens, nstep=15000, burnin=5000, thin=1, mhalo_prior=
     return outdic
 
 
+def fit_nfwdev_mstarmhaloprior(lens, nstep=15000, burnin=5000, thin=1, mhalo_prior=(13., 1., 0.5), mstar_prior=(11.5, 0.5), alpha_prior=(0., 0.2), cvir_prior=(0.877, 0.3)):
+
+    model_lens = lens_models.NfwDev(zd=lens.zd, zs=lens.zs, mstar=lens.mstar, mhalo=lens.mhalo, \
+                                    reff_phys=lens.reff_phys, cvir=lens.cvir, images=lens.images, source=lens.source)
+
+    model_lens.normalize()
+
+    xa_obs, xb_obs = lens.obs_images[0]
+    imerr = lens.obs_images[1]
+
+    mstar_obs, mstar_err = lens.obs_lmstar
+
+    radmagrat_obs, radmagrat_err = lens.obs_radmagrat
+
+    model_lens.get_caustic()
+    model_lens.make_grids(err=imerr, nsig=5.)
+
+    mstar_par = pymc.Normal('lmstar', mu=mstar_prior[0], tau=1./mstar_prior[1]**2, value=np.log10(lens.mstar))
+    alpha_par = pymc.Normal('alpha', mu=alpha_prior[0], tau=1./alpha_prior[1]**2, value=0.)
+    mhalo_par = pymc.Normal('mhalo', mu=mhalo_prior[0] + mhalo_prior[1]*(mstar_par - 11.5), tau=1./mhalo_prior[2]**2, value=np.log10(lens.mhalo))
+    c_par = pymc.Normal('lcvir', mu=cvir_prior[0], tau=1./cvir_prior[1]**2, value=np.log10(lens.cvir))
+
+    @pymc.deterministic()
+    def caustic(mstar=mstar_par, mhalo=mhalo_par, cvir=c_par):
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = 10.**cvir
+
+        model_lens.normalize()
+
+        model_lens.get_caustic()
+
+        return model_lens.caustic
+
+    s2_par = pymc.Uniform('s2', lower=0., upper=caustic**2, value=lens.source**2)
+
+    @pymc.deterministic()
+    def images(mstar=mstar_par, mhalo=mhalo_par, s2=s2_par, cvir=c_par):
+
+        model_lens.source = s2**0.5
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = 10.**cvir
+
+        model_lens.normalize()
+
+        model_lens.get_images()
+        if len(model_lens.images) < 2:
+            return np.inf, -np.inf
+        else:
+            return model_lens.images
+
+    @pymc.deterministic()
+    def image_a(imgs=images):
+        return imgs[0]
+
+    @pymc.deterministic()
+    def image_b(imgs=images):
+        return imgs[1]
+
+    @pymc.deterministic()
+    def radmagrat(mstar=mstar_par, mhalo=mhalo_par, s2=s2_par, cvir=c_par, imgs=images):
+
+        model_lens.source = s2**0.5
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = 10.**cvir
+
+        model_lens.normalize()
+        model_lens.images = imgs
+
+        if not np.isfinite(imgs[0]):
+            return 0.
+        else:
+            model_lens.get_radmag_ratio()
+            return model_lens.radmag_ratio
+
+    @pymc.deterministic()
+    def timedelay(mstar=mstar_par, mhalo=mhalo_par, s2=s2_par, cvir=c_par, imgs=images):
+
+        model_lens.source = s2**0.5
+        model_lens.mstar = 10.**mstar
+        model_lens.mhalo = 10.**mhalo
+        model_lens.cvir = 10.**cvir
+
+        model_lens.normalize()
+        model_lens.images = imgs
+
+        if not np.isfinite(imgs[0]):
+            return 0.
+        else:
+            model_lens.get_timedelay()
+            return model_lens.timedelay
+
+    ima_logp = pymc.Normal('ima_logp', mu=image_a, tau=1./imerr**2, value=xa_obs, observed=True)
+    imb_logp = pymc.Normal('imb_logp', mu=image_b, tau=1./imerr**2, value=xb_obs, observed=True)
+
+    msps_logp = pymc.Normal('msps_logp', mu=mstar_par-alpha_par, tau=1./mstar_err**2, value=mstar_obs, observed=True)
+
+    radmag_logp = pymc.Normal('radmag_logp', mu=radmagrat, tau=1./radmagrat_err**2, value=radmagrat_obs, observed=True)
+
+    pars = [mstar_par, mhalo_par, alpha_par, c_par, s2_par, timedelay, image_a, image_b, caustic, radmagrat]
+
+    M = pymc.MCMC(pars)
+    M.use_step_method(pymc.AdaptiveMetropolis, [mstar_par, mhalo_par, alpha_par, c_par, s2_par])
+    M.sample(nstep, burnin, thin=thin)
+
+    outdic = {'mstar':M.trace('lmstar')[:], 'mhalo':M.trace('mhalo')[:], 'alpha': M.trace('alpha')[:], \
+              'lcvir': M.trace('lcvir')[:], 'timedelay':M.trace('timedelay')[:], \
+              'source':(M.trace('s2')[:]**0.5).flatten(), 'image_a':M.trace('image_a')[:], \
+              'image_b':M.trace('image_b')[:], 'caustic': M.trace('caustic')[:], 'radmagrat': M.trace('radmagrat')[:], 'mhalo_prior': mhalo_prior, 'mstar_prior': mstar_prior, 'alpha_prior': alpha_prior, 'cvir_prior': cvir_prior}
+
+    return outdic
+
+
 def fit_nfwdev_interimprior_flatcprior(lens, nstep=15000, burnin=5000, thin=1, mhalo_prior=(13., 0.5), mstar_prior=(11.5, 0.5), alpha_prior=(0., 0.2), cvir_prior=(0.5, 1.5)):
 
     model_lens = lens_models.NfwDev(zd=lens.zd, zs=lens.zs, mstar=lens.mstar, mhalo=lens.mhalo, \
