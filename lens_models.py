8f7d7470471e11f7b66862b36e3756fd69cb7090
Alessandro Sonnenfeld <alessandro.sonnenfeld@ipmu.jp>
2017-08-22 00:42:53 +0900

    added gnfw with mdm within re as free parameter

--- lens_models.py
+++ lens_models.py
@@ -1,5 +1,5 @@
 import numpy as np
-from mass_profiles import sersic as sersic_profile, NFW as NFW_profile, deV_mlgrad as deV_mlgrad_profile
+from mass_profiles import sersic as sersic_profile, NFW as NFW_profile, deV_mlgrad as deV_mlgrad_profile, gNFW as gNFW_profile
 from scipy.optimize import brentq, minimize_scalar
 from allZeTools import cgsconstants as cgs
 from cosmolopy import distance, density
@@ -9,7 +9,6 @@ from scipy.special import hyp2f1
 default_cosmo = {'omega_M_0': 0.3, 'omega_lambda_0': 0.7, 'omega_k_0': 0., 'h': 0.7}
 
 eps = 1e-15
-
 
 class NfwDev:
     
@@ -72,6 +71,191 @@ class NfwDev:
     def alpha(self, x):
         r = abs(x)
         return self.halo*NFW_profile.M2d(r, self.rs)/x/np.pi + \
+               self.mstar/self.S_cr*sersic_profile.fast_M2d(r, 4., self.reff)/x/np.pi
+
+    def mu_r(self, x):
+        return (1. + self.m(x)/x**2 - 2.*self.kappa(x))**(-1)
+
+    def mu_t(self, x):
+        return (1. - self.m(x)/x**2)**(-1.)
+
+    def mu(self, x):
+        return self.mu_r(x) * self.mu_t(x)
+
+    def get_caustic(self):
+
+        rmin = self.reff/50.
+        rmax = 10.*self.reff
+
+        radial_invmag = lambda r: 2.*self.kappa(r) - self.m(r)/r**2 - 1.
+
+        if radial_invmag(rmin)*radial_invmag(rmax) > 0.:
+            rcrit = rmin
+        else:
+            rcrit = brentq(radial_invmag, rmin, rmax)
+
+        ycaust = -(rcrit - self.alpha(rcrit))
+        self.caustic = ycaust
+        self.radcrit = rcrit
+
+    def get_rein(self):
+
+        tangential_invmag = lambda r: self.m(r)/r**2 - 1.
+
+        if tangential_invmag(self.reff/50.) < 0.:
+            self.rein = 0.
+        elif tangential_invmag(10.*self.reff) > 0.:
+            self.rein = 10.*self.reff
+        else:
+            tcrit = brentq(tangential_invmag, self.reff/50., 10.*self.reff)
+            self.rein = tcrit
+
+    def get_xy_minmag(self, min_mag=0.5):
+
+        self.get_rein()
+        self.get_caustic()
+
+        eps = 1e-4*self.rein
+
+        # finds the minimum magnification between the radial and tangential critical curves
+        self.magmin = minimize_scalar(self.mu, bounds=(self.radcrit+eps, self.rein-eps), method='Bounded').x
+
+        if abs(self.mu(self.magmin)) > min_mag:
+            self.xminmag = self.radcrit
+            self.yminmag = self.caustic
+        else:
+            self.xminmag = brentq(lambda x: abs(self.mu(x)) - min_mag, self.magmin, self.rein-eps, xtol=eps)
+            self.yminmag = -self.xminmag + self.alpha(self.xminmag)
+
+    def get_images(self, xtol=1e-8):
+
+        rmin = self.reff/50.
+        rmax = 10.*self.reff
+
+        imageeq = lambda r: r - self.alpha(r) - self.source
+        if imageeq(self.radcrit)*imageeq(rmax) >= 0. or imageeq(-rmax)*imageeq(-self.radcrit) >= 0.:
+            self.images = (-np.inf, np.inf)
+        else:
+            xa = brentq(imageeq, rmin, rmax, xtol=xtol)
+            xb = brentq(imageeq, -rmax, -self.radcrit, xtol=xtol)
+            self.images = (xa, xb)
+
+    def get_timedelay(self):
+        self.timedelay = -self.Dt/cgs.c*cgs.arcsec2rad**2*(0.5*(self.images[0]**2 - self.images[1]**2) - self.images[0]*self.source + \
+                                   self.images[1]*self.source - self.lenspot(self.images[0]) + \
+                                   self.lenspot(-self.images[1]))/(self.h/default_cosmo['h'])
+
+    def make_grids(self, err=0.01, nsig=3.):
+        tol = 0.1*err
+        x0A = self.images[0] - nsig*err
+        x0A = x0A - x0A%tol
+        x1A = self.images[0] + nsig*err
+        x1A = x1A - x1A%tol
+        gridA = np.arange(x0A, x1A, tol)
+
+        x0B = self.images[1] - nsig*err
+        x0B = x0B - x0B%tol
+        x1B = -max(self.rs/500.,-(self.images[1] + nsig*err))#, self.radcrit)
+        x1B = x1B - x1B%tol
+        gridB = np.arange(x0B,x1B,tol)
+
+        self.grids = (gridA,gridB)
+
+    def make_rein_grid(self,err=0.01,nsigma=5.):
+        tol = 0.1*err
+        r0 = max(self.rs/500., self.rein - nsigma*err)
+        r1 = self.rein + nsigma*err
+        grid = np.arange(r0,r1,tol)
+
+        self.rein_grid = grid
+
+    def fast_images(self):
+
+        self.images = []
+        for grid in self.grids:
+            dx = grid[1] - grid[0]
+            ys = grid - self.alpha(grid)
+            diff = (self.source - ys[:-1])*(self.source - ys[1:])
+            found = diff <= 0.
+            for x in grid[:-1][found]:
+                self.images.append(x+0.5*dx)
+
+    def fast_rein(self):
+
+        ys = self.rein_grid - self.alpha(self.rein_grid)
+        
+        rein = abs(ys).argmin()
+        
+        self.rein = self.rein_grid[rein]
+
+    def get_radmag_ratio(self):
+        self.radmag_ratio = self.mu_r(self.images[0])/self.mu_r(self.images[1])
+
+    def get_gammap(self):
+        self.gammap = 3. - (4.*np.pi*self.reff**3)*(self.mhalo*NFW_profile.rho(self.reff, self.rs)/NFW_profile.M3d(self.rvir, self.rs) + self.mstar*sersic_profile.rho(self.reff,4., self.reff))/(self.mhalo*NFW_profile.M3d(self.reff, self.rs)/NFW_profile.M3d(self.rvir, self.rs) + self.mstar*0.4135)
+
+
+class gNfwDev:
+    
+    def __init__(self, zd=0.3, zs=2., h=0.7, mstar=1e11, mdme=1e11, beta=1., reff_phys=1., rs_phys=10., images=[], source=0., \
+                 obs_images=None, obs_lmstar=None, obs_radmagrat=None, obs_timedelay=None, delta_halo=200.):
+
+        self.zd = zd
+        self.zs = zs
+        self.h = h
+        self.halo = None
+        self.mstar = mstar
+        self.mdme = mdme
+        self.beta = beta
+        self.rs_phys = rs_phys
+        self.delta_halo = delta_halo
+        self.reff_phys = reff_phys
+        self.caustic = None
+        self.radcrit = None
+        self.source = source
+        self.images = images
+        self.timedelay = None
+        self.grids = None
+        self.radmag_ratio = None
+        self.dyndic = None
+        self.rein = None
+        self.imA = None
+        self.imB = None
+        self.rvir = None
+        self.rs = None
+        self.gammap = None
+        self.obs_lmstar = obs_lmstar
+        self.obs_radmagrat = obs_radmagrat
+        self.obs_images = obs_images
+        self.obs_timedelay = obs_timedelay
+
+        self.ds = distance.angular_diameter_distance(self.zs, **default_cosmo)
+        self.dds = distance.angular_diameter_distance(self.zs, self.zd, **default_cosmo)
+        self.dd = distance.angular_diameter_distance(self.zd, **default_cosmo)
+
+        self.S_cr = cgs.c**2/(4.*np.pi*cgs.G)*self.ds*self.dd/self.dds*cgs.Mpc/cgs.M_Sun*cgs.arcsec2rad**2
+        self.arcsec2kpc = cgs.arcsec2rad*self.dd*1000.
+        self.reff = self.reff_phys/self.arcsec2kpc
+        self.rs = self.rs_phys/self.arcsec2kpc
+        self.Dt = self.dd*self.ds/self.dds*cgs.Mpc*(1. + self.zd)
+        self.rhoc = density.cosmo_densities(**default_cosmo)[0]*distance.e_z(self.zd, **default_cosmo)**2
+ 
+    def normalize(self):
+        self.halo = self.mdme/gNFW_profile.fast_M2d(self.reff, self.rs, self.beta)/self.S_cr
+
+    def kappa(self, x):
+        return self.halo*gNFW_profile.Sigma(abs(x), self.rs, self.beta) + self.mstar/self.S_cr*sersic_profile.I(abs(x), 4., self.reff)
+
+    def m(self, x):
+        return self.halo*gNFW_profile.fast_M2d(abs(x), self.rs, self.beta)/np.pi + \
+               self.mstar/self.S_cr*sersic_profile.fast_M2d(abs(x), 4., self.reff)/np.pi
+
+    def lenspot(self, r):
+        return self.halo*gNFW_profile.fast_lenspot(r, self.rs, self.beta) + self.mstar/self.S_cr*sersic_profile.fast_lenspot(r, 4., self.reff)
+
+    def alpha(self, x):
+        r = abs(x)
+        return self.halo*gNFW_profile.fast_M2d(r, self.rs, self.beta)/x/np.pi + \
                self.mstar/self.S_cr*sersic_profile.fast_M2d(r, 4., self.reff)/x/np.pi
 
     def mu_r(self, x):
@@ -977,7 +1161,7 @@ class broken_alpha_powerlaw: #spherical power-law with a break at the Einstein r
         return self.alpha(x) * x
 
     def kappa(self, x):
-        return 2.**(self.beta - 1.) * ((3.-self.gamma)*(x/self.rein)**(1.-self.gamma)*(1.+x/self.rein)**(-self.beta) - self.beta * (x/self.rein)**(2.-self.gamma)*(1.+x/self.rein)**(-1.-self.beta))
+        return 2.**(self.beta - 1.) * ((3.-self.gamma)*(abs(x)/self.rein)**(1.-self.gamma)*(1.+abs(x)/self.rein)**(-self.beta) - self.beta * (abs(x)/self.rein)**(2.-self.gamma)*(1.+abs(x)/self.rein)**(-1.-self.beta))
 
     def lenspot(self, x):
         return 2.**self.beta/(3.-self.gamma)*self.rein**2 * (x/self.rein)**(3.-self.gamma) * hyp2f1(3.-self.gamma, self.beta, 4.-self.gamma, -x/self.rein)
